9517f54f13cc46d5c572b182d66bd1b6e2902f36
451360508@qq.com <451360508@qq.com>
2017-06-08 13:55:48 +0800

    mul ballview mode

---
+++ app/src/main/java/com/season/playball/sin/BallView.java
@@ -0,0 +1,84 @@
+package com.season.playball.sin;
+
+import android.content.Context;
+import android.graphics.Canvas;
+import android.os.Handler;
+import android.view.View;
+
+import java.util.ArrayList;
+import java.util.List;
+
+
+/**
+ * Disc:
+ * User: SeasonAllan(451360508@qq.com)
+ * Time: 2017-06-08 11:59
+ */
+public class BallView extends View{
+
+    public BallView(Context context) {
+        super(context);
+        ballList = new ArrayList<>();
+    }
+
+
+    List<BallModel> ballList;
+
+
+    private boolean running = true;
+    int time = 10;
+    public void stop() {
+        running = false;
+        handler.removeMessages(1);
+    }
+
+    public void start() {
+        running = true;
+        for (BallModel ballModel:ballList)
+            ballModel.move();
+        crashCheck();
+        invalidate();
+        handler.sendEmptyMessageDelayed(1, time);
+    }
+
+    void crashCheck() {
+        for (BallModel currentBall : ballList) {
+            for (BallModel checkBall : ballList) {
+                if (currentBall.id != checkBall.id) {
+                    if (currentBall.isCrash(checkBall)) {
+                        currentBall.crashChanged(checkBall);
+                    }
+                }
+            }
+        }
+    }
+
+
+    private Handler handler = new Handler() {
+        public void handleMessage(android.os.Message msg) {
+            if (running) {
+                start();
+            }
+        };
+    };
+
+    @Override
+    protected void onDraw(Canvas canvas) {
+        super.onDraw(canvas);
+        for (BallModel ballModel:ballList)
+            ballModel.draw(canvas);
+    }
+
+
+    public void clear() {
+        ballList.clear();
+    }
+
+    public void add(View parentView) {
+        BallModel ballModel = new BallModel(parentView.getWidth(), parentView.getHeight());
+        ballModel.id = System.currentTimeMillis();
+        ballModel.randomSetUp();
+        ballList.add(ballModel);
+    }
+
+}
